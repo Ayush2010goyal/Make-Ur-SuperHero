b00294675d8b99e1afe6cf1e976aceb391c9e3eb
Ayush2010goyal <75526409+Ayush2010goyal@users.noreply.github.com>
2020-12-19 16:52:49 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,71 @@
+window.addEventListener("keydown",my_keydown);
+
+function my_keydown (e){
+    keyPressed=e.keyCode;
+    console.log(keyPressed);
+
+    if(e.shiftKey == true && keyPressed=='80'){
+        console.log("p and shift pressed Together");
+         block_image_width=block_image_width+10;
+         block_image_height=block_image_height+10;
+         document.getElementById("current_width").innerHTML=block_image_width;
+         document.getElementById("current_height").innerHTML=block_image_height;
+    }
+
+    if(e.shiftKey == true && keyPressed=='77'){
+        console.log("m and shift pressed Together");
+         block_image_width=block_image_width-10;
+         block_image_height=block_image_height-10;
+         document.getElementById("current_width").innerHTML=block_image_width;
+         document.getElementById("current_height").innerHTML=block_image_height;
+    }
+    if( keyPressed=='38'){
+        up();
+        console.log("up");
+    }
+
+    if( keyPressed=='40'){
+        down();
+        console.log("down");
+    }
+
+    if( keyPressed=='37'){
+        left();
+        console.log("left");
+    }
+
+    if( keyPressed=='39'){
+        right();
+        console.log("right");
+    }
+
+    if( keyPressed=='72'){
+        new_image('captain_america_left_hand.png');
+        console.log("CaptainAmericaShield");
+    }
+
+    if( keyPressed=='76'){
+        new_image('hulk_legs.png');
+        console.log("HulkFace");
+    }
+
+    if( keyPressed=='76'){
+        new_image('hulk_left_hand.png');
+        console.log("hulk_left_hand");
+    }
+
+    if( keyPressed=='70'){
+        new_image('ironman_face.png');
+        console.log("t");
+    }
+
+    if( keyPressed=='82'){
+        new_image('thor_left_hand.png');
+        console.log("r");
+    }
+
+    if( keyPressed=='66'){
+        new_image('spiderman_body.png');
+        console.log("y");
+    }
+}
